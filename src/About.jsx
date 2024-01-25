6fb53e8f767fc88e94524c0231c8d3a72c8fb3cf
Barkha Yadav <barkhayadav128@gmail.com>
2024-01-25 11:01:04 +0530

    changes made

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./index.css";
-import img from "./photos/abg.jpeg";
 const About = () => {
   return (
     <div className="aboutContainer">
@@ -8,24 +7,25 @@ const About = () => {
         <h1 style={{ color: "violet" }}>A little about me,</h1>
         <h5 style={{marginTop:'10px'}}>Hello!</h5>
         <p>
-          My name is Barkha Yadav. I am a 2nd year undergraduate at NIT,
-          Manipur.
-        </p>
-        <p>
-          An avid learner with interest in programming and software development.
-        </p>
-        <p>I am tech geek and likes to explore new technology and softwares.</p>
-        <p>I love problem solving and my field of interest are Web development and App development.</p>
-        <p>
-          My hobbies are watching anime,reading manga,gaming and I like making
-          diy sometimes :)
-        </p>
+        I'm Barkha Yadav, a 2nd year undergraduate at NIT,Manipur.Whenever
+        possible, I love learning new technologies and developing new projects.I develop web applications
+        mostly using the MERN stack, but I'm also exploring other technologies and frameworks that are interesting to me!
+        Additionally, I am studying Mobile App Development.I am passionate about technology, and I love to contribute to the world of technology.
+          </p>
       </div>
-      <div className="tech">
+      <h1 class='tech' style={{color:'violet'}}>Find me on</h1>
+      <div className='links tech'>
+        <a href='https://x.com/flyhighalwayss?t=JGuyeGggng0kI15bq1K3Bg&s=09'><i class="bi bi-twitter-x"></i></a>
+        <a href='https://in.linkedin.com/in/barkha-yadav-727472212'><i class="bi bi-linkedin"></i></a>
+        <a href='https://github.com/barkhayadav12'><i class="bi bi-github"></i></a>
+        <a href="mailto:barkhayadav125@gmail.com"><i class="bi bi-envelope-at"></i></a>
+        </div>
+      <div className="tech" style={{marginTop:'70px'}}>
         <h2 style={{color:'violet'}}>Tech I'm familiar with</h2>
         <p>C++</p>
         <p>C</p>
         <p>Python</p>
+        <p>Dart</p>
         <p>HTML</p>
         <p>CSS</p>
         <p>JavaScript</p>
@@ -35,7 +35,7 @@ const About = () => {
         <p>Ejs</p>
         <p>MySQL</p>
         <p>MongoDB</p>
-
+        <p>Flutter</p>
 
       </div>
       <div></div>
