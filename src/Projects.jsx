cd14ac0893ee4ca436265503c9f45407513fb150
Barkha Yadav <barkhayadav128@gmail.com>
2024-01-29 11:49:40 +0530

    added project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import solowhisper from "./photos/soloWhisperr.jpeg";
+import soloWhisperr from "./photos/soloWhisper.png";
 import crypto from "./photos/crypto.jpeg";
 import chat from "./photos/chat.jpeg";
 import jokes from "./photos/jokes.jpeg";
 import plannar from "./photos/plannar.jpeg";
-import slider from "./photos/slider.jpeg";
+
 import adopt from './photos/adopt.jpeg'
+import codeHelpp from './photos/codeHelp.png'
 const Projects = () => {
+  const codeHelp = {
+    img: "",
+    title: "CodeHelp",
+    description:
+      "Developed an application where the users can write, read articles on programming and ask their doubts when they run into an error.",
+    link:'https://github.com/barkhayadav12/CodeHelp_CodingHelp',
+    tech: "Tech stack: Ejs,Bootstrap,Node.js,Express.js,MongoDB.",
+  };
   const soloWhisper = {
     img: "",
     title: "SoloWhisper",
@@ -53,98 +62,111 @@ const Projects = () => {
   };
   return (
     <div class="projectContainer ">
-      <h1 style={{ textAlign: "center" ,color:'white'}}>My Recent <span style={{color:'violet'}}>Works</span></h1>
-      <p style={{ textAlign: "center" ,color:'white'}}>
-        Here are few projects I've worked on recently.
-      </p>
-      <div className="majorProjects">
-        <div className="projects">
+    <h1 style={{ textAlign: "center" ,color:'white'}}>My Recent <span style={{color:'violet'}}>Works</span></h1>
+    <p style={{ textAlign: "center" ,color:'white'}}>
+      Here are few projects I've worked on recently.
+    </p>
+    <div className="majorProjects">
+    <div className="projects">
+        <div className="imgProjects">
+          <img src={codeHelpp} />
+        </div>
+        <h3>{codeHelp.title}</h3>
+        <p>{codeHelp.description}</p>
+        <h6>{codeHelp.tech}</h6>
+        <div className="btnProjects">
+          <button className="btnP">
+            <a href={codeHelp.link}><i class="bi bi-github"></i>  GitHub</a>
+          </button>
+        </div>
+        </div>
+    <div className="projects">
           <div className="imgProjects">
-            <img src={solowhisper} />
+            <img src={adopt} />
           </div>
-          <h3>{soloWhisper.title}</h3>
-          <p>{soloWhisper.description}</p>
-          <h6>{soloWhisper.tech}</h6>
+          <h3>{cryptoTracker.title}</h3>
+          <p>{cryptoTracker.description}</p>
+          <h6>{cryptoTracker.tech}</h6>
           <div className="btnProjects">
             <button className="btnP">
-              <a href={soloWhisper.link}><i class="bi bi-github"></i>  GitHub</a>
+              <a href='https://github.com/barkhayadav12/AdoptMeHooman'><i class="bi bi-github"></i> GitHub</a>
             </button>
           </div>
+        </div>
+        <div className="projects">
+          <div className="imgProjects">
+            <img src={chat} />
           </div>
-          <div className="projects">
-            <div className="imgProjects">
-              <img src={chat} />
-            </div>
-            <h3>{chatRoom.title}</h3>
-            <p>{chatRoom.description}</p>
-            <h6>{chatRoom.tech}</h6>
-            <div className="btnProjects">
-              <button className="btnP">
-                <a href='https://github.com/barkhayadav12/ChatRoom'><i class="bi bi-github"></i> GitHub</a>
-              </button>
-            </div>
-          </div>
-          <div className="projects">
-            <div className="imgProjects">
-              <img src={adopt} />
-            </div>
-            <h3>{cryptoTracker.title}</h3>
-            <p>{cryptoTracker.description}</p>
-            <h6>{cryptoTracker.tech}</h6>
-            <div className="btnProjects">
-              <button className="btnP">
-                <a href='https://github.com/barkhayadav12/AdoptMeHooman'><i class="bi bi-github"></i> GitHub</a>
-              </button>
-            </div>
+          <h3>{chatRoom.title}</h3>
+          <p>{chatRoom.description}</p>
+          <h6>{chatRoom.tech}</h6>
+          <div className="btnProjects">
+            <button className="btnP">
+              <a href='https://github.com/barkhayadav12/ChatRoom'><i class="bi bi-github"></i> GitHub</a>
+            </button>
           </div>
         </div>
-        <div>
-          <h1 style={{ padding: "10px",marginLeft:'12px' }} className="mini">
-            Mini Projects
-          </h1>
-          <div className="majorProjects">
-            <div className="projects">
-              <div className="imgProjects">
-                <img src={crypto} />
-              </div>
-              <h3>{imageSlider.title}</h3>
-              <p>{imageSlider.description}</p>
-              <h6>{imageSlider.tech}</h6>
-              <div className="btnProjects">
-                <button className="btnP">
-                  <a href='https://github.com/barkhayadav12/CryptoTracker'><i class="bi bi-github"></i> GitHub</a>
-                </button>
-              </div>
+      </div>
+      <div>
+        <h1 style={{ padding: "10px",marginLeft:'12px' }} className="mini">
+          Mini Projects
+        </h1>
+        <div className="majorProjects">
+        <div className="projects">
+        <div className="imgProjects">
+          <img src={soloWhisperr} />
+        </div>
+        <h3>{soloWhisper.title}</h3>
+        <p>{soloWhisper.description}</p>
+        <h6>{soloWhisper.tech}</h6>
+        <div className="btnProjects">
+          <button className="btnP">
+            <a href={soloWhisper.link}><i class="bi bi-github"></i>  GitHub</a>
+          </button>
+        </div>
+        </div>
+          <div className="projects">
+            <div className="imgProjects">
+              <img src={crypto} />
             </div>
-            <div className="projects">
-              <div className="imgProjects">
-                <img src={plannar} />
-              </div>
-              <h3>{studyPlannar.title}</h3>
-              <p>{studyPlannar.description}</p>
-              <h6>{studyPlannar.tech}</h6>
-              <div className="btnProjects">
-                <button className="btnP">
-                  <a href='https://github.com/barkhayadav12/StudyPlannar'><i class="bi bi-github"></i> GitHub</a>
-                </button>
-              </div>
+            <h3>{imageSlider.title}</h3>
+            <p>{imageSlider.description}</p>
+            <h6>{imageSlider.tech}</h6>
+            <div className="btnProjects">
+              <button className="btnP">
+                <a href='https://github.com/barkhayadav12/CryptoTracker'><i class="bi bi-github"></i> GitHub</a>
+              </button>
             </div>
-            <div className="projects">
-              <div className="imgProjects">
-                <img src={jokes} />
-              </div>
-              <h3>{guessGame.title}</h3>
-              <p>{guessGame.description}</p>
-              <h6>{guessGame.tech}</h6>
-              <div className="btnProjects">
-                <button className="btnP">
-                  <a href='https://github.com/barkhayadav12/RandomJokes'><i class="bi bi-github"></i> GitHub</a>
-                </button>
-              </div>
+          </div>
+          <div className="projects">
+            <div className="imgProjects">
+              <img src={plannar} />
+            </div>
+            <h3>{studyPlannar.title}</h3>
+            <p>{studyPlannar.description}</p>
+            <h6>{studyPlannar.tech}</h6>
+            <div className="btnProjects">
+              <button className="btnP">
+                <a href='https://github.com/barkhayadav12/StudyPlannar'><i class="bi bi-github"></i> GitHub</a>
+              </button>
+            </div>
+          </div>
+          <div className="projects">
+            <div className="imgProjects">
+              <img src={jokes} />
+            </div>
+            <h3>{guessGame.title}</h3>
+            <p>{guessGame.description}</p>
+            <p>{guessGame.tech}</p>
+            <div className="btnProjects">
+              <button className="btnP">
+                <a href='https://github.com/barkhayadav12/RandomJokes'><i class="bi bi-github"></i> GitHub</a>
+              </button>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
